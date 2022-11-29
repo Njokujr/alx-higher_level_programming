e80bf49196ae3883d67982fecd99bbd61790de25
njokujr <njokuvictory216@gmail.com>
2022-11-29 10:38:59 -0800

    1**

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -14,8 +14,10 @@ if x < 0:
 
 if lastDigit > 5:
     string = "and is greater than 5"
+
 elif lastDigit == 0:
     string = "and is 0"
+
 elif lastDigit < 6:
     string = "and is less than 6 and not 0"
 
