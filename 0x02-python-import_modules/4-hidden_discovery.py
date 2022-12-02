e1b16e069ba8e6ab4425f38bb73487bcff7b48ef
Njokujr <111055423+Njokujr@users.noreply.github.com>
2022-12-01 20:26:04 -0800

    mandatory_taks

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+import hidden_4
+
+if __name__ == '__main__':
+
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '-':
+            print(name)
