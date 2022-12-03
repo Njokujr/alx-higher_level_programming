9aa3f105694a999484235a202130046160b8bdf8
njokujr <njokuvictory216@gmail.com>
2022-12-03 01:03:50 -0800

    *1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
     '''
     Gets an element in a list at given index
     And returns it
     '''
-#!/usr/bin/python3
-def element_at(my_list, idx):
     if idx >= len(my_list) or idx < 0:
         return
 
