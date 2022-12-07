0895d6305638a20be496876d01de687315d00031
njokujr <njokuvictory216@gmail.com>
2022-12-07 10:57:55 -0800

    *12-roman_num

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-=======
-def roman_to_int(roman_string):
-
-    if roman_string is None or type(roman_string) is not str:
-        return total
->>>>>>> 8f030be4bfb342cd53d05bd39ac887f05757fb04
-
 
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -35,4 +27,4 @@ def roman_to_int(roman_string):
 
         i = i + 1
 
-    return tota
+    return total
