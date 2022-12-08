6cd0111fc3dd87dd2180e3711d6aac3cdcf1c3c1
Njokujr <111055423+Njokujr@users.noreply.github.com>
2022-12-07 19:48:46 -0800

    *12-roman_num

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
-        return (0)
+        return total
+
     roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                   'D': 500, 'M': 1000}
-    roman_list = list(roman_string.upper())
-    result = 0
-    prev = 0
-    for letter in roman_list:
-        if letter in roman_dictionary:
-            result += roman_dictionary[letter]
-            if roman_dictionary[letter] > prev:
-                result -= prev * 2
-            prev = roman_dictionary[letter]
+    total = 0
+    i = 0
+
+    while i in range(len(roman_string)):
+
+        if i == len(roman_string) - 1:
+            total += roman_dictionary[roman_string[i]]
+            break
+
         else:
-            return (0)
-    return (result)
+            if roman_dictionary[roman_string[i]] < roman_dictionary[roman_string[i+1]]:
+                total += roman_dictionary[roman_string[i+1]] - roman_dictionary[roman_string[i]]
+                i = i + 1
+
+                if i + 1 == len(roman_string):
+                    break
+
+            else:
+                total += roman_dictionary[roman_string[i]]
+
+        i = i + 1
+
+    return total
