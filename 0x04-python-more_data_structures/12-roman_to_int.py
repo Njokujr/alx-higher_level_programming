c659883c0d67149c410f16072c751dbbaf7f1abc
Njokujr <111055423+Njokujr@users.noreply.github.com>
2022-12-07 19:54:30 -0800

    *12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return total
 
-    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                  'D': 500, 'M': 1000}
+
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     i = 0
+
+    if (roman_string is None) or (type(roman_string) is not str):
+        return total
 
     while i in range(len(roman_string)):
 
         if i == len(roman_string) - 1:
-            total += roman_dictionary[roman_string[i]]
+            total += roman[roman_string[i]]
             break
 
         else:
-            if roman_dictionary[roman_string[i]] < roman_dictionary[roman_string[i+1]]:
-                total += roman_dictionary[roman_string[i+1]] - roman_dictionary[roman_string[i]]
+            if roman[roman_string[i]] < roman[roman_string[i+1]]:
+                total += roman[roman_string[i+1]] - roman[roman_string[i]]
                 i = i + 1
 
                 if i + 1 == len(roman_string):
                     break
 
             else:
-                total += roman_dictionary[roman_string[i]]
+                total += roman[roman_string[i]]
 
         i = i + 1
 
-    return total
+    return tota
