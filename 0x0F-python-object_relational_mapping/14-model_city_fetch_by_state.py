41dbac2adf1c30e715c9d554502c584aa0678d03
njokujr <njokuvictory216@gmail.com>
2023-03-19 18:28:05 -0700

    task-14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,23 +8,13 @@ if __name__ == '__main__':
     from sqlalchemy import asc, create_engine
     from sqlalchemy.orm import sessionmaker
 
-
-    if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 4:
-        print("Usage: {} username password database_name".format(args[0]))
-        exit(1)
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, data))
-    # create custom session object class from database engine
+    _, user, passwd, db = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user,
+                           passwd, db))
     Session = sessionmaker(bind=engine)
-    # create instance of new custom session class
     session = Session()
-    results = session.query(State.name, City.id, City.name)\
-                     .join(City, City.state_id == State.id)\
-                     .order_by(City.id)
-    for result in results:
-        print("{}: ({}) {}".format(result[0], result[1], result[2]))
+
+    cities = session.query(City).order_by(asc(City.id)).all()
+    for city in cities:
+        print('{}: ({}) {}'.format(city.state.name, city.id, city.name))
+    session.close()
