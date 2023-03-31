ba298c9e2e43e371802f8ac1438e523d6102f0ce
njokujr <njokuvictory216@gmail.com>
2023-03-31 11:17:04 -0700

    task-0..check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+A Script that fetches the status of
+url: https://alx-intranet.hbtn.io/status
+"""
 
 import urllib.request
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
+
+    print('Body Response:')
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- content: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
