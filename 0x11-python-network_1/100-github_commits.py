59238531c0035cc5b32c4213d47b95f6e3936b4c
njokujr <njokuvictory216@gmail.com>
2023-03-31 15:22:27 -0700

    advanced-task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,16 +4,12 @@
 import requests
 import sys
 
-# Get the repository name and owner name from command-line arguments
 repo_name = sys.argv[1]
 owner_name = sys.argv[2]
+response = requests.get(f"https: // api.github.com/repos
+                        / {owner_name}/{repo_name}")
 
-# Make a GET request to the GitHub API to retrieve information about the repository
-response = requests.get(f"https://api.github.com/repos/{owner_name}/{repo_name}")
-
-# Check the status code of the response to see if the request was successful
 if response.status_code == 200:
-    # If the request was successful, print some information about the repository
     repo_info = response.json()
     print(f"Repository name: {repo_info['name']}")
     print(f"Owner name: {repo_info['owner']['login']}")
@@ -21,6 +17,5 @@ if response.status_code == 200:
     print(f"Number of stars: {repo_info['stargazers_count']}")
     print(f"Number of forks: {repo_info['forks_count']}")
 else:
-    # If the request was not successful, print an error message
-    print(f"Error: Could not retrieve information about {owner_name}/{repo_name}")
-
+    print(f"Error: Could not retrieve information about
+          {owner_name}/{repo_name}")
