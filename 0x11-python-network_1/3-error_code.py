c72193116d5ab3a4529471b6c772845d18084357
njokujr <njokuvictory216@gmail.com>
2023-03-31 12:52:08 -0700

    task--checks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,12 +6,11 @@ import urllib.request
 import sys
 
 if __name__ == '__main__':
-    url = sys.argv[1:]
+    url = sys.argv[1]
 
     try:
-        with urllib.request.urlopen(url) as response:
-            res = response.read().decode('utf-8')
-            print(res)
+        with urllib.request.urlopen(url) as r:
+            print(r.read().decode('utf-8'))
     except urllib.error.URLError as e:
         if hasattr(e, 'code'):
             print('Error code: {}'.format(e.code))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1:]
     r = requests.get(url)
-    res = r.header.get('X-Request-Id')
+    res = r.headers.get('X-Request-Id')
     print(res)
