f5a25be59338575af8f55d26d49f9b531425dae9
njokujr <njokuvictory216@gmail.com>
2023-03-31 10:59:44 -0700

    task-0....check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
